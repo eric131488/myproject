ee41a57a1dce4cec9747d7a2f318682796ce4198
Eric <eric@bonraybio.com>
2020-11-18 22:29:29 +0800

    initial commit

---
+++ src/main/kotlin/com/hank/Hello.kt
@@ -0,0 +1,14 @@
+package com.hank
+
+fun main() {
+//    println("Hello kotlin")
+//    Human().hello()
+    val human = Human()
+    human.hello()
+}
+
+class Human {
+    fun hello() {
+        print("Hello kotlin")
+    }
+}
